7efc9856edb20f5bf8aec9ee71a984b713119902
Rasmus <KokholmGames@hotmail.com>
2015-03-25 22:15:39 +0100

    migrations

--- database/migrations/2015_03_25_210627_create_list_user_table.php
+++ database/migrations/2015_03_25_210627_create_list_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateListsTable extends Migration {
+class CreateListUserTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,9 +12,9 @@ class CreateListsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('lists', function(Blueprint $table)
-		{
-			$table->increments('id');
+		Schema::create('list_user', function(Blueprint $table){
+			$table->integer('list_id');
+			$table->integer('user_id');
 			$table->timestamps();
 		});
 	}
@@ -26,7 +26,7 @@ class CreateListsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('lists');
+		Schema::drop('list_user');
 	}
 
 }
